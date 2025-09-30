7710a097b40c47d617c82c55b82713fce3198da1
LittlePien <abdulmalikfajar14@gmail.com>
2025-10-01 03:14:43 +0800

    Add files via upload

---
+++ Prak103_2410817210020_AMalikFajarPutraSyamsi.java
@@ -0,0 +1,34 @@
+package prak100;
+
+import java.util.Scanner;
+
+public class Prak103_2410817210020_AMalikFajarPutraSyamsi {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Input: ");
+		int rowNum = input.nextInt();
+		int currentNum = input.nextInt();
+		
+		int i = 0;
+		
+		do {
+			if(currentNum % 2 != 0) {
+				System.out.print(currentNum);
+				
+				i++;
+				
+				if(i < rowNum) {
+					System.out.print(", ");
+				}
+			}
+			
+			currentNum++;
+			
+		} while(i < rowNum);
+		
+		input.close();
+	}
+	
+}
---
+++ Prak105_2410817210020_AMalikFajarPutraSyamsi.java
@@ -0,0 +1,28 @@
+package prak100;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Prak105_2410817210020_AMalikFajarPutraSyamsi {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Masukkan jari-jari: ");
+		double radius = input.nextDouble();
+		
+		System.out.print("Masukkan tinggi: ");
+		double height = input.nextDouble();
+		
+		double volume = 3.14 * radius * radius * height;
+		String volumeFormat = String.format("%.3f", volume);
+		
+		System.out.print("Volume tabung dengan jari-jari " + radius + " cm dan tinggi " + height + " cm adalah " + volumeFormat + " m3");
+		
+		input.close();
+
+	}
+
+}
